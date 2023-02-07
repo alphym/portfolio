d50916e6b45b4c448b35cd9fff1cfcba3201ce43
alphym <alphy108@gmail.com>
2023-02-08 00:46:32 +0200

    update style and inde.html

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const menuBar = document.querySelector('.bar');
+const closeMenu = document.querySelector('.close-menu');
+const subMenu = document.querySelector('.sub-menu');
+const mainBar = document.querySelector('.main-bar');
+
+
+menuBar.addEventListener('click', function(){
+    menuBar.classList.add("no-active");
+    closeMenu.classList.remove("no-active");
+    subMenu.classList.add("active");
+    mainBar.style.backgroundColor = '#6070FF';
+    mainBar.style.position = 'fixed';
+    mainBar.style.top = '0px';
+    mainBar.style.zIndex = '99';
+
+})
+closeMenu.addEventListener('click', function () {
+    closeMenu.classList.add("no-active");
+    menuBar.classList.remove("no-active");
+    subMenu.classList.remove("active");
+    mainBar.style.backgroundColor = 'white';
+    mainBar.style.position = 'static';
+    mainBar.style.zIndex = '0';
+
+})
+
+
+
+
+
