4d1b9f1402fba4926f60aede9c79d00a6b4750fa
alphym <alphy108@gmail.com>
2023-02-08 23:02:16 +0200

    update script.js

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ const mainBar = document.querySelector('.main-bar');
 
 
 menuBar.addEventListener('click', function(){
-    menuBar.classList.add("no-active");
-    closeMenu.classList.remove("no-active");
-    subMenu.classList.add("active");
+    menuBar.classList.add('no-active');
+    closeMenu.classList.remove('no-active');
+    subMenu.classList.add('active');
     mainBar.style.backgroundColor = '#6070FF';
     mainBar.style.position = 'fixed';
     mainBar.style.top = '0px';
     mainBar.style.zIndex = '99';
 
-})
+});
 closeMenu.addEventListener('click', function () {
-    closeMenu.classList.add("no-active");
-    menuBar.classList.remove("no-active");
-    subMenu.classList.remove("active");
+    closeMenu.classList.add('no-active');
+    menuBar.classList.remove('no-active');
+    subMenu.classList.remove('active');
     mainBar.style.backgroundColor = 'white';
     mainBar.style.position = 'static';
     mainBar.style.zIndex = '0';
 
-})
+});
 
 
 
