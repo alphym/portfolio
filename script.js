4a58ae0dc6c982c79d6f1dac2b806460727af6fa
alphym <alphy108@gmail.com>
2023-02-08 23:09:24 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
 const menuBar = document.querySelector('.bar');
 const closeMenu = document.querySelector('.close-menu');
 const subMenu = document.querySelector('.sub-menu');
 const mainBar = document.querySelector('.main-bar');
 
-
-menuBar.addEventListener('click', function(){
+menuBar.addEventListener('click', function (){
     menuBar.classList.add('no-active');
     closeMenu.classList.remove('no-active');
     subMenu.classList.add('active');
@@ -13,8 +11,8 @@ menuBar.addEventListener('click', function(){
     mainBar.style.position = 'fixed';
     mainBar.style.top = '0px';
     mainBar.style.zIndex = '99';
-
 });
+
 closeMenu.addEventListener('click', function () {
     closeMenu.classList.add('no-active');
     menuBar.classList.remove('no-active');
