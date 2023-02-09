d3e5e5e9296eb7f12753cb70dc25d768cc9b1a94
alphym <alphy108@gmail.com>
2023-02-09 18:32:27 +0200

    update files and js linters

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 const menuBar = document.querySelector('.bar');
-const closeMenu = document.querySelector('.close-menu');
 const subMenu = document.querySelector('.sub-menu');
-const mainBar = document.querySelector('.main-bar');
+const closeMenu = document.querySelector('.sub-menu .close-menu');
 
-menuBar.addEventListener('click', function (){
-    menuBar.classList.add('no-active');
-    closeMenu.classList.remove('no-active');
-    subMenu.classList.add('active');
-    mainBar.style.backgroundColor = '#6070FF';
-    mainBar.style.position = 'fixed';
-    mainBar.style.top = '0px';
-    mainBar.style.zIndex = '99';
+const mainBarContent = document.querySelector('.main-bar');
+const mainContent = document.querySelector('.main-content');
+const MainSection = document.querySelector('.main-main');
+
+menuBar.addEventListener('click', () => {
+  subMenu.classList.add('active');
+  mainBarContent.classList.add('blur');
+  mainContent.classList.add('blur');
+  MainSection.classList.add('blur');
 });
 
-closeMenu.addEventListener('click', function () {
-    closeMenu.classList.add('no-active');
-    menuBar.classList.remove('no-active');
-    subMenu.classList.remove('active');
-    mainBar.style.backgroundColor = 'white';
-    mainBar.style.position = 'static';
-    mainBar.style.zIndex = '0';
-
+closeMenu.addEventListener('click', () => {
+  subMenu.classList.remove('active');
+  mainBarContent.classList.remove('blur');
+  mainContent.classList.remove('blur');
+  MainSection.classList.remove('blur');
 });
-
-
-
-
-
