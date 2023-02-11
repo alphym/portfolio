5832bbc9623acf73c287b9476c1981ef17620f13
alphym <alphy108@gmail.com>
2023-02-11 22:04:21 +0200

    add validation on conatct form

--- script.js
+++ script.js
@@ -221,3 +221,81 @@ closeMenu.addEventListener('click', () => {
   mainContent.classList.remove('blur');
   MainSection.classList.remove('blur');
 });
+
+// contact form validation code
+function showMessage(input, message, type) {
+  // get the small element and set the message
+  const msg = input.parentNode.querySelector('small');
+  msg.innerText = message;
+  // update the class for the input
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check if the value is not empty
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email format
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const email = input.value.trim();
+  if (!emailRegex.test(email) || email.toLowerCase() !== email) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+// validate email
+const form = document.querySelector('#signup');
+const formTwo = document.querySelector('#signup-two');
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const TEXT_MESSAGE = 'Please enter your message';
+const EMAIL_INVALID = 'Please enter a correct email address format';
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // validate the form
+  const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(
+    form.elements.email,
+    EMAIL_REQUIRED,
+    EMAIL_INVALID,
+  );
+  const textValid = hasValue(form.elements.message, TEXT_MESSAGE);
+  // if valid, submit the form.
+  if (nameValid && emailValid && textValid) {
+    form.submit();
+    form.reset();
+  }
+});
+formTwo.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // validate the form
+  const nameValid = hasValue(formTwo.elements.name, NAME_REQUIRED);
+  const emailValid = validateEmail(
+    formTwo.elements.email,
+    EMAIL_REQUIRED,
+    EMAIL_INVALID,
+  );
+  const textValid = hasValue(formTwo.elements.message, TEXT_MESSAGE);
+  // if valid, submit the form.
+  if (nameValid && emailValid && textValid) {
+    formTwo.submit();
+    formTwo.reset();
+  }
+});
+
+// end validate email
